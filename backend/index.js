1a2c31344aadaac7f5edc64ded33b9454283c848
Smitgajera10 <smitgajera10@gmail.com>
2025-04-14 17:15:24 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,37 @@
 import express, { json } from "express";
-import { connect } from "mongoose";
 import cors from "cors";
 import { config } from "dotenv";
-import eventRouts from "./src/routes/event.js";
-import checklistRoutes from "./src/routes/checklist.js"
-import authRoutes from "./src/routes/auth.js";
 import serverless from "serverless-http";
+import eventRouts from "./src/routes/event.js";
+import checklistRoutes from "./src/routes/checklist.js";
+import authRoutes from "./src/routes/auth.js";
+import { connectToDB } from "./src/utils/db.js";
 
-config()
+config();
+
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(cors(
-  {
-    origin: "https://packpall-9113dw2d8-smitgajera10s-projects.vercel.app", // your frontend URL
+app.use(
+  cors({
+    origin: "https://packpall-9113dw2d8-smitgajera10s-projects.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true // if you're sending cookies or authorization headers
-  }
-));
+    credentials: true
+  })
+);
+
 app.use(json());
-// Routes
+
+app.use(async (req, res, next) => {
+  await connectToDB(); // Ensures DB is connected only once per cold start
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRouts);
 app.use("/api/checklists", checklistRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello from Serverless!");
 });
-
-connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("MongoDB connection error:", err));
-
 
 export default serverless(app);
